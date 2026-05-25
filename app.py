4e06e28a8df5536e7ebc61086874b79a475f4609
kabolandia-alt <kabolandia@gmail.com>
2026-05-24 20:48:39 -0400

    Recrear base de datos con nueva estructura

--- app.py
+++ app.py
@@ -391,6 +391,7 @@ def validar_pago(reserva_id):
     return jsonify({'error': 'No encontrada'}), 404
 
 with app.app_context():
+    db.drop_all()
     db.create_all()
     if not Usuario.query.filter_by(username='admin').first():
         posada = Posada(nombre='Demo-Posadas', direccion='Sistema de gestión de prueba')
